ddb5c93d87f9536ab3d4bd27b12e7207c8e865dc
juanvegab <juanvegab23@gmail.com>
2013-09-25 16:14:55 -0600

    Project init, Se trata de Vinos

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('setratadevinos.comApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
